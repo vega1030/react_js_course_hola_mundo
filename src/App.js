572e9b7e97d5afa5248edd36d3c7aad5daf505a8
vega1030 <fvega.3010@gmail.com>
2023-01-09 13:06:33 +0100

    feat: :zap: maps and keys at the elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import Button from "./Button.js";
 
+//arrays and your keys
+
+const arr = [
+    'chanchito feliz',
+    'chanchito triste',
+    'chanchito emocionado'
+]
+
 const App = () => {
+
+    const validate = false
+    if (validate) {
+        return <p>Mi variable es true</p>
+    }
+
     return (
         <div>
 
-            <h1>Hola Mundo</h1>
+            <h1 onClick={ (e) => console.log('click', e) }>
+                Hola Mundo
+            </h1>
+            {/* se debe pasar un key para identificar cada elemento p */}
+            { arr.map(el => <p key={el}>{ el }</p>) }
+
             <Button onClick={ () => console.log('clickeado') }> Enviar </Button>
         </div>
     )
