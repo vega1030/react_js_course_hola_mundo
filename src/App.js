0aed64fcfa8a278a78d275ef9487b923ada47504
vega1030 <fvega.3010@gmail.com>
2023-01-11 13:26:47 +0100

    feat: :zap: create components with class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { Component } from 'react'
 
+class Button extends Component {
+    render() {
+        console.log('ejecutando metodo render de button')
+        return (
+            <button >
+                Enviar
+            </button>
+        )
+    }
+}
+
 class App extends Component {
     state = {
-        valor:3
+        valor: 3
     }
     render() {
         console.log(this.state)
         return (
             <div>
                 <p>Hola Mundo</p>
-                <button 
-                className={`${this.state.valor}`}
-                onClick={ () => this.setState({ valor: 2 }) }>
-                    Enviar
+                <button
+                    className={ `${ this.state.valor }` }
+                    onClick={ () => this.setState({ valor: 2 }) }>
+                    Enviar en App
                 </button>
             </div>
         )
