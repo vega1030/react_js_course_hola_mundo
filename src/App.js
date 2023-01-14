0f4fecfe15dda1e610c5f46c4444c62316e11dbd
vega1030 <fvega.3010@gmail.com>
2023-01-15 00:02:41 +0100

    fix: :bug: fix error at state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import { Component } from 'react'
 
-class Button extends Component {
+class Input extends Component {
+
     render() {
-        console.log('ejecutando metodo render de button')
         return (
-            <button >
-                Enviar
-            </button>
+            <input
+                value={ this.props.value }
+                onChange={ this.props.onChange }
+            />
         )
     }
 }
-
 class App extends Component {
     state = {
-        valor: 3
+        nombre: '',
+        apellido: ''
     }
+
+    updateValues = (prop, value) => {
+        this.setState({ [ prop ]: value })
+    }
+
     render() {
-        console.log(this.state)
         return (
-            <div>
-                <p>Hola Mundo</p>
-                <button
-                    className={ `${ this.state.valor }` }
-                    onClick={ () => this.setState({ valor: 2 }) }>
-                    Enviar en App
-                </button>
-            </div>
+            <p>
+                Nombre completo: { `${ this.state.nombre } ${ this.state.apellido }` }
+
+                <Input
+                    value={ this.state.nombre }
+                    onChange={ e => this.updateValues('nombre', e.target.value) }
+                />
+                <Input
+                    value={ this.state.apoellido }
+                    onChange={ e => this.updateValues('apellido', e.target.value) }
+                />
+            </p>
         )
     }
 }
