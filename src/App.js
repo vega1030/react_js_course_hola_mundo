b232dab79d3674660a81f510339200d13d88967c
vega1030 <fvega.3010@gmail.com>
2023-01-17 23:06:06 +0100

    feat: :sparkles: complete shopping cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Products from './components/Products.jsx'
+import Layout from './components/Layout.jsx'
+import Title from "./components/Title.jsx";
+import Navbar from "./components/Navbar.jsx";
+class App extends Component {
+  state = {
+    products: [ {
+      name: 'Tomate', price: 1500, img: '/image/tomate.jpg'
+    },
+    {
+      name: 'Arbejas', price: 2500, img: '/image/arbejas.jpg'
+    },
+    { name: 'Lechuga', price: 500, img: '/image/lechuga.jpg' }
+    ],
+    cart:[],
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    displayCart:false,
+  }
+  addCart = (product)=>{
+    const { cart } = this.state
+    if (cart.find(x=>x.name ===product.name)) {
+      const newCart = cart.map(x=>x.name===product.name?({
+        ...x,
+        quant:x.quant+1
+      }):x)
+      return this.setState({ cart: newCart})
+    }
+    return this.setState({
+      cart: this.state.cart.concat({
+        ...product,
+        quant:1
+      })
+    })
+  }
+
+  viewCart= ()=>{
+    if (!this.state.cart.length) {
+      return
+    }
+    this.setState({displayCart:!this.state.displayCart})
+  }
+
+  render() {
+    const { displayCart } = this.state
+    return (
+      <div>
+        <Navbar cart={this.state.cart} 
+        displayCart={displayCart} 
+        viewCart={this.viewCart}/>
+        <Layout>
+          <Title />
+          <p>{ this.state.products.price }</p>
+{/* Calling the Products component and passing the addCart and products props. */}
+          <Products
+            addCart={ this.addCart}
+            products={ this.state.products }
+          />
+        </Layout>
+
+      </div>
+    );
+  }
 }
 
 export default App;
